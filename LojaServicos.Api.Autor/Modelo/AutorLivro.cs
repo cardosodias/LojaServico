35f195f7333bc5176164e21d3c78a40ee1316476
vibe_ecardoso <vibe_ecardoso@SUSIS_VIBE_030>
2021-01-14 16:47:39 -0300

    Subida do projeto

---
+++ LojaServicos.Api.Autor/Modelo/AutorLivro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaServicos.Api.Autor.Modelo
+{
+    public class AutorLivro
+    {
+        public int AutorLivroId { get; set; }
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public ICollection<GrauAcademico> ListaGrauAcademico { get; set; }
+        public string AutorLivroGuid { get; set; }
+    }
+}
---
+++ LojaServicos.Api.Autor/Modelo/GrauAcademico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaServicos.Api.Autor.Modelo
+{
+    public class GrauAcademico
+    {
+        public int GrauAcademicoId { get; set; }
+        public string Nome { get; set; }
+        public string CentroAcademico { get; set; }
+        public DateTime? DataGrau { get; set; }
+        public int AutorLivroId { get; set; }
+        public AutorLivro AutorLivro { get; set; }
+        public string GrauAcademicoGuid { get; set; }
+
+    }
+}
