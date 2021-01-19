4a412e651ba0ce4415742bee65d285f0e251a257
vibe_ecardoso <vibe_ecardoso@SUSIS_VIBE_030>
2021-01-19 14:08:34 -0300

    Mudanças

---
+++ LojaServicos.Api.Autor/Persistencia/ContextoAutor.cs
@@ -0,0 +1,17 @@
+﻿using LojaServicos.Api.Autor.Modelo;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaServicos.Api.Autor.Persistencia
+{
+    public class ContextoAutor : DbContext
+    {
+        public ContextoAutor(DbContextOptions<ContextoAutor> options) : base(options) { }
+
+        public DbSet<AutorLivro> AutorLivro { get; set; }
+        public DbSet<GrauAcademico> GrauAcademico { get; set; }
+    }
+}
--- LojaServicos.Api.Autor/Startup.cs
+++ LojaServicos.Api.Autor/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LojaServicos.Api.Autor.Persistencia;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,11 @@ namespace LojaServicos.Api.Autor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ContextoAutor>(option =>
+            {
+                option.UseNpgsql(Configuration.GetConnectionString("ConexaoDataBase"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
