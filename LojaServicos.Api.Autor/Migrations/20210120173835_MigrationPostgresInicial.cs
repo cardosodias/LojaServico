772979d0fa7c8a31929135f26b80e0d7d8b28cbf
vibe_ecardoso <vibe_ecardoso@SUSIS_VIBE_030>
2021-01-20 17:17:09 -0300

    evolução do projeto

---
+++ LojaServicos.Api.Autor/Migrations/20210120173835_MigrationPostgresInicial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace LojaServicos.Api.Autor.Migrations
+{
+    public partial class MigrationPostgresInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AutorLivro",
+                columns: table => new
+                {
+                    AutorLivroId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Apelido = table.Column<string>(nullable: true),
+                    DataNascimento = table.Column<DateTime>(nullable: true),
+                    AutorLivroGuid = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AutorLivro", x => x.AutorLivroId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GrauAcademico",
+                columns: table => new
+                {
+                    GrauAcademicoId = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    CentroAcademico = table.Column<string>(nullable: true),
+                    DataGrau = table.Column<DateTime>(nullable: true),
+                    AutorLivroId = table.Column<int>(nullable: false),
+                    GrauAcademicoGuid = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GrauAcademico", x => x.GrauAcademicoId);
+                    table.ForeignKey(
+                        name: "FK_GrauAcademico_AutorLivro_AutorLivroId",
+                        column: x => x.AutorLivroId,
+                        principalTable: "AutorLivro",
+                        principalColumn: "AutorLivroId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GrauAcademico_AutorLivroId",
+                table: "GrauAcademico",
+                column: "AutorLivroId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GrauAcademico");
+
+            migrationBuilder.DropTable(
+                name: "AutorLivro");
+        }
+    }
+}
